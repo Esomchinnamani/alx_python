15d7c722f04eba1ab210da0928efd823c840566d
Esomchinoel <esomedu@gmail.com>
2023-10-02 23:48:54 +0100

    python data structures project tasks solved

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+# define a function that prints a matrix of integers
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, element in enumerate(row):
+            print("{:d}".format(element), end=" " if i < len(row) - 1 else '')
+        print()
