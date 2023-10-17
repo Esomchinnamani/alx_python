b82b998d4db5d34b410104c08bcc58c221201136
Esomchinoel <esomedu@gmail.com>
2023-10-17 17:06:12 +0100

    python network What's my status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script: network_status_checker.py
+A simple script to check the status of a website using the 'requests' library.
+
+Author: [Your Name]
+
+Usage:
+    Run this script using the command: python network_status_checker.py
+"""
+
+
+import requests
+
+r = 'https://alu-intranet.hbtn.io/status'
+
+try:
+    response = requests.get(r)
+    response.raise_for_status()
+
+    print(f"Body response:")
+    print("\t- type: {}".format(type(r)))
+    print("\t- content: {}".format(response.text))
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred while trying to access {r}: {e}")
